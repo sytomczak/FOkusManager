ebd2ad62c56b9260093cbb2183194b7e7bf01a9f
sytomczak <sylwia.tomczak95@gmail.com>
2020-10-08 16:16:44 +0200

    Setted size legendJPanel, calendarJPanel and notesJPanel in CalendarMainView.form

--- src/main/java/pl/sytomczak/fokusmanager/calender/mainView/CalendarMainView.java
+++ src/main/java/pl/sytomczak/fokusmanager/calender/mainView/CalendarMainView.java
@@ -1,5 +1,10 @@
 package pl.sytomczak.fokusmanager.calender.mainView;
 
+import com.mindfusion.common.DateTime;
+import com.mindfusion.scheduling.CalendarView;
+import com.mindfusion.scheduling.CustomDrawElements;
+import com.mindfusion.scheduling.awt.AwtCalendar;
+import org.w3c.dom.Text;
 import pl.sytomczak.fokusmanager.calender.monthsview.ActualMonthView;
 
 import javax.swing.*;
@@ -10,36 +15,31 @@ import java.awt.event.KeyEvent;
 
 public class CalendarMainView extends JFrame {
     private JPanel mainJPanel;
-    private JPanel legendJPanel;
     private JPanel calendarJPanel;
     private JPanel notesJPanel;
     private JTextField notesTitle;
-    private JTextField legendTitle;
     private JTextField calendarTitle;
     private JRadioButton yearsRadioButton;
     private JRadioButton newNotesRadioButton;
-    private JRadioButton newCategoryRadioButton;
     private JPanel clockJPane;
     private JPanel actualNameMonthPanel;
+    private JPanel legendJPanel;
+    private JTextField legendTextField;
 
     public CalendarMainView() {
         setContentPane(mainJPanel);
         setTitle("FocusManager");
 
-        mainJPanel.setPreferredSize(new Dimension(1400, 800));
+        mainJPanel.setPreferredSize(new Dimension(1400, 900));
         mainJPanel.registerKeyboardAction(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 onCancel();
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 
-        ActualMonthView actualMonthView  = new ActualMonthView();
-        actualMonthView.getContentPane().add(calendarJPanel);
-     //   calendarJPanel.add(mainJPanel);
-        //mainJPanel.add(calendarJPanel);
-
 
     }
+
 
     private void onCancel() {
         dispose();
