63ec85e0f0f4a54f3bb5e1f69a0f38b5033be2c5
sytomczak <sylwia.tomczak95@gmail.com>
2020-10-20 13:32:05 +0200

    Added buttons in NotesView.form

--- src/main/java/pl/sytomczak/fokusmanager/notes/notesview/NotesView.java
+++ src/main/java/pl/sytomczak/fokusmanager/notes/notesview/NotesView.java
@@ -10,11 +10,19 @@ public class NotesView extends JFrame {
     private JTextArea notesArea;
     private JPanel notesJPanel;
     private JEditorPane titlePanel;
+    private JButton newNoteButton;
+    private JButton saveButton;
+    private JButton cutButton;
+    private JButton copyButton;
+    private JButton pasteButton;
+    private JButton selectAllButton;
+    private JTextField textField1;
+    private JButton searchButton;
 
 
     public NotesView() {
 
-        setSize(400, 400);
+        setSize(510, 450);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
         setLocationRelativeTo(null);
