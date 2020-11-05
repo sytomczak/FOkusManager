2756b2efde127f35b5a3994e202ab78e47e31f93
sytomczak <sylwia.tomczak95@gmail.com>
2020-11-05 14:04:28 +0100

    Cleared code in CalendarMainView

--- src/main/java/pl/sytomczak/fokusmanager/calendar/mainView/CalendarMainView.java
+++ src/main/java/pl/sytomczak/fokusmanager/calendar/mainView/CalendarMainView.java
@@ -1,7 +1,5 @@
 package pl.sytomczak.fokusmanager.calendar.mainView;
 
-import com.mindfusion.drawing.Colors;
-import org.joda.time.Days;
 import pl.sytomczak.fokusmanager.calendar.Clock;
 import pl.sytomczak.fokusmanager.calendar.months.ActualMonth;
 import pl.sytomczak.fokusmanager.calendar.months.ActualYear;
@@ -10,17 +8,11 @@ import pl.sytomczak.fokusmanager.dbutils.DBConnection;
 import pl.sytomczak.fokusmanager.notes.notesview.NotesView;
 
 import javax.swing.*;
-import javax.swing.border.Border;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.sql.Connection;
-import java.time.Month;
-import java.time.Year;
-import java.time.YearMonth;
-import java.util.Calendar;
 
 public class CalendarMainView extends JFrame {
     private JPanel mainJPanel;
@@ -75,15 +67,6 @@ public class CalendarMainView extends JFrame {
         ActualYear.getActualYear(yearsPanel);
 
         controlsInActualMonth();
-
-//        JButton dayButton = new JButton();
-//        dayButton.setBackground(Color.yellow);
-//        calendarJPanel.setLayout(null);
-//     //   dayButton.setSize(80, 80);
-//        dayButton.setText(String.valueOf(1));
-//        dayButton.setBounds(623, 113, 80, 80);
-//        calendarJPanel.add(dayButton);
-
     }
 
 
@@ -92,59 +75,31 @@ public class CalendarMainView extends JFrame {
     }
 
     private void controlsInActualMonth() throws Exception {
-
-
         for (int i = 0; i <= months.daysInMonth(); i++) {
-        JButton dayButton = new JButton();
-        dayButton.setBackground(Color.yellow);
-        calendarJPanel.setLayout(null);
-          dayButton.setSize(80, 80);
-        dayButton.setText(String.valueOf(i +1));
-        dayButton.setBounds(623 + i+70 , 113 + i+70, 80, 80);
-        calendarJPanel.add(dayButton);
-        i = i++;
+            JButton dayButton = new JButton();
+            dayButton.setBackground(Color.yellow);
+            calendarJPanel.setLayout(null);
+            dayButton.setSize(80, 80);
+            dayButton.setText(String.valueOf(i + 1));
+            dayButton.setBounds(623 + i + 70, 113 + i + 70, 80, 80);
+            calendarJPanel.add(dayButton);
+            i = i++;
         }
-//
-//        int i = 0;
-
-//        do {
-//            try {
-//                JButton dayButton = new JButton();
-//                dayButton.setBackground(Color.yellow);
-//                dayButton.setSize(10, 10);
-//                //dayButton.setLayout(null);
-//                dayButton.setName(String.valueOf(i));
-//                JLabel lb = new JLabel();
-//                lb.setText("asdasdasd");
-//                if(calendarJPanel != null)
-//                    calendarJPanel.add(lb); //dayButton);
-//                i++;
-//            } catch (Exception ex) {
-//                String s = "";
-//            }
-//        }
-//        while (i >= 3);
     }
 
 
-            public static void main (String[]args) throws Exception {
-                CalendarMainView calendarMainView = new CalendarMainView();
-                calendarMainView.pack();
-                calendarMainView.setResizable(false);
-                calendarMainView.setVisible(true);
-                calendarMainView.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-                calendarMainView.setLocationRelativeTo(null);
+    public static void main(String[] args) throws Exception {
+        CalendarMainView calendarMainView = new CalendarMainView();
+        calendarMainView.pack();
+        calendarMainView.setResizable(false);
+        calendarMainView.setVisible(true);
+        calendarMainView.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        calendarMainView.setLocationRelativeTo(null);
 
-                Connection connection = DBConnection.getConnection();
+        Connection connection = DBConnection.getConnection();
 
-//                int year = YearMonth.now().getYear();
-//                int month = YearMonth.now().getMonthValue();
-//
-//
-//                YearMonth yearMonthObject = YearMonth.of(year, month);
-//                int daysInMonth = yearMonthObject.lengthOfMonth();
-                System.out.println(calendarMainView.months.daysInMonth());
-            }
+        System.out.println(calendarMainView.months.daysInMonth());
+    }
 
 
-        }
+}
