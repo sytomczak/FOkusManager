0769aea5d4a77ff38d7882f81560a0a7f19b438a
sytomczak <sylwia.tomczak95@gmail.com>
2020-11-30 20:01:58 +0100

    Created NoteThumbnail view

--- src/main/java/pl/sytomczak/fokusmanager/notes/notethumbnail/NoteThumbnail.java
+++ src/main/java/pl/sytomczak/fokusmanager/notes/notethumbnail/NoteThumbnail.java
@@ -1,4 +1,9 @@
 package pl.sytomczak.fokusmanager.notes.notethumbnail;
 
+import javax.swing.*;
+
 public class NoteThumbnail {
+    private JTextArea titleTextArea;
+    private JTextArea noteTextArea;
+    private JPanel thumbnailPanel;
 }
